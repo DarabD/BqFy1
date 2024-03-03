5e23ed263875273f42c3b8d857a5515fb6f5ab79
Muhammad Darab <154218161+DarabD@users.noreply.github.com>
2024-03-03 19:28:51 +0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
-import SignIn from './pages/Signin';
+import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import { auth } from './firebase';
 
